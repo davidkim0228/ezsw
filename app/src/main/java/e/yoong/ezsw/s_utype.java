d9655abbfa2ba1a45391c5c710d28f4c4a982778
yoong song <yoong9627@gmail.com>
2018-02-16 22:49:07 -0800

    update

--- app/src/main/java/e/yoong/ezsw/s_utype.java
+++ app/src/main/java/e/yoong/ezsw/s_utype.java
@@ -1,11 +1,8 @@
 package e.yoong.ezsw;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
 
 public class s_utype extends AppCompatActivity {
@@ -14,17 +11,6 @@ public class s_utype extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_s_utype);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     public void transfer_next(){
