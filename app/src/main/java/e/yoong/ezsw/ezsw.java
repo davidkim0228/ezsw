e2ee2271fdc7b4fb1e0dadbb777836245a4ec626
yoong song <yoong9627@gmail.com>
2018-02-17 02:34:53 -0800

    database

--- app/src/main/java/e/yoong/ezsw/ezsw.java
+++ app/src/main/java/e/yoong/ezsw/ezsw.java
@@ -1,9 +1,16 @@
 package e.yoong.ezsw;
 
+import android.app.Application;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 
 public class ezsw extends AppCompatActivity {
 
@@ -17,6 +24,10 @@ public class ezsw extends AppCompatActivity {
         startActivity(intent);
     }
     public void submitclicked(View v) {
+
+        ParseObject abc = new ParseObject("Request");
+        abc.put("myNumber", "123");
+        abc.saveInBackground();
         transfer_next();
     }
 }
